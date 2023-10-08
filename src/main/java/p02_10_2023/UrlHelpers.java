b19351b82f6c8aa54cdddb947dac39c9bcf60b10
Tamara Sejic <sejic.tamara@gmail.com>
2023-10-08 17:27:12 +0200

    Update UrlHelpers Class with cookie handler metode.

--- src/main/java/p02_10_2023/UrlHelpers.java
+++ src/main/java/p02_10_2023/UrlHelpers.java
@@ -1,13 +1,19 @@
 package p02_10_2023;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.*;
 
 public class UrlHelpers {
 
     public static int getHTTPResponseStatusCode(String urlStr) throws IOException {
 
+        URL url = new URL(urlStr);
+        HttpURLConnection http = (HttpURLConnection)url.openConnection();
+        return http.getResponseCode();
+    }
+
+    public static int getHTTPResponseStatusCodeCookieHandler(String urlStr) throws IOException {
+        CookieHandler.setDefault(new CookieManager(null, CookiePolicy.ACCEPT_ALL));
         URL url = new URL(urlStr);
         HttpURLConnection http = (HttpURLConnection)url.openConnection();
         return http.getResponseCode();
